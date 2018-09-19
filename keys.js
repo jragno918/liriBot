c8542ae7186a993b5bdbb22a0168aa77a77732a7
Joe Ragno <joseph.ragno3@gmail.com>
2018-09-19 15:14:01 -0400

    liri updates

--- keys.js
+++ keys.js
@@ -1,13 +1,14 @@
 console.log('this is loaded');
 
-exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-};
+exports.twitterKeys = {
+  consumer_key: 'mbg1foZtVoYsrIZ8MI6oASa9n',
+  consumer_secret: 'DUh47moCTCw3lx6ye3Gio9o2oOt8izsJUmdSPcQDj7sbkcdRU3',
+  access_token_key: '757763396972445696-IXzAYCtbkd9MiF7h9xEZjxdRxGq6i1P',
+  access_token_secret: 'GEaGG6XP0vXwd6pD3MAYERClJsRWsheEV4LRzhMRaRluA',
+}
 
-exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-};
+
+exports.spotifyKeys = {
+	id: "5a8fdf523e5647c88795f0e6d5f530cf",
+	secret: "9bd02873e1a845e6b828a45bf9e6a943"
+}
--- liri.js
+++ liri.js
@@ -1,12 +1,123 @@
-var request = require("request");
+// Dependencies
+var keys = require('./keys.js');
+var Twitter = require('twitter');
+var request = require('request');
+var fs = require("fs");
 
-// We then run the request module on a URL with a JSON
-request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
+// Spotify dependencies
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotifyKeys);
 
-  // If there were no errors and the response code was 200 (i.e. the request was successful)...
-  if (!error && response.statusCode === 200) {
+// Twitter command
+var getMyTweets = function() {
+  var client = new Twitter(keys.twitterKeys);
 
-    // Then we print out the imdbRating
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+  var params = {
+    screen_name: '_angrbrd'
+  };
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+
+
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].created_at);
+        console.log(' ');
+        console.log(tweets[i].text);
+
+      }
+    }
+  });
+}
+
+// Spotify command
+var spotifyThis = function() {
+
+  spotify.search({
+    type: 'track',
+    query: 'dancing in the moonlight'
+  }, function(err, data) {
+    if (err) {
+      console.log('Error occurred: ' + err);
+      return;
+
+    };
+
+    console.log(data.tracks.items[0]);
+
+    for (var x = 0; x < 20; x++) {
+      console.log("Songs Information #" + (x + 1),
+        "\nName: " + data.tracks.items[x].name,
+        "\nArtist: " + data.tracks.items[x].artists[0].name,
+        "\nAlbum: " + data.tracks.items[x].album.name,
+        "\nPreview URL: " + data.tracks.items[x].preview_url + "\n");
+    }
+  });
+
+
+};
+
+// IMDb command
+var getMeMovie = function(movieName) {
+
+  request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy",
+    function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var jsonData = JSON.parse(body);
+
+        console.log("Title: " + jsonData.Title);
+        console.log("Year: " + jsonData.Year);
+        console.log("Rated: " + jsonData.Rated);
+        console.log("IMDB Rating: " + jsonData.imdbRating);
+        console.log("Country: " + jsonData.Country);
+        console.log("Language: " + jsonData.Language);
+        console.log("Plot: " + jsonData.Plot);
+        console.log("Actors: " + jsonData.Actors);
+        console.log("Rotten Tomatoes Rating: " + jsonData.tomatoRating);
+        console.log("Rotton Tomatoes URL: " + jsonData.tomatoURL);
+      }
+    });
+};
+
+//
+var doWhatItSays = function() {
+
+  fs.readFile('random.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+
+    var dataArr = data.split(',');
+
+    if (dataArr.length == 2) {
+      pick(dataArr[0], dataArr[1]);
+    } else if (dataArr.length == 1) {
+      pick(dataArr[0]);
+    }
+
+  });
+}
+
+// Commands for LiriBot
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+    case 'my-tweets':
+      getMyTweets();
+      break;
+    case 'my-spotify':
+      spotifyThis();
+      break;
+    case 'movie-this':
+      getMeMovie();
+    case 'do-what-it-says':
+      doWhatItSays();
+      break;
+    default:
+      console.log('LIRI does not know that');
   }
-});
+}
+
+//
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
